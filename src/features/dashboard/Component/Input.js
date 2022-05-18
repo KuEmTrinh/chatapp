b5367e178a8d18a2b29294cd4882573e2cbe9722
KuEmTrinh <91253322+KuEmTrinh@users.noreply.github.com>
2022-05-18 15:29:28 +0900

    part 4

--- src/features/dashboard/Component/Input.js
+++ src/features/dashboard/Component/Input.js
@@ -1,7 +1,63 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+import "./Input.css";
+import { db } from "../../../app/firebase";
+import { firebase } from "../../../app/firebase";
 export default function Input() {
+  const [inputValue, setInputValue] = useState("");
+  const [userInfomation, setUserInfomation] = useState("");
+  const changeValue = (e) => {
+    setInputValue(e.target.value);
+  };
+  const sendMessage = () => {
+    console.log(inputValue);
+    console.log(userInfomation.displayName)
+  };
+  const setInfo = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUserInfomation(user);
+      }
+    });
+  }
+  useEffect(()=> {
+    setInfo();
+  },[])
   return (
-    <div>Input</div>
-  )
+    <div className="inputPlace">
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "50ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="standard-textarea"
+          label="Message"
+          placeholder="input message"
+          multiline
+          variant="standard"
+          onChange={changeValue}
+        />
+      </Box>
+
+      <Stack alignItems="center">
+        <Button
+          variant="outlined"
+          href="#outlined-buttons"
+          onClick={() => {
+            sendMessage();
+          }}
+        >
+          Send
+        </Button>
+      </Stack>
+    </div>
+  );
 }
